bf540f2878774e2fcd66c20a4639b761dbe16750
umeraziz1002 <umeraziz1002@gmail.com>
2023-10-15 10:14:52 +0500

    update

--- Qr Code.py
+++ Qr Code.py
@@ -1,7 +1,7 @@
 from tkinter import *
+from tkinter.ttk import *
 import qrcode
 import cv2
-
 
 root = Tk()
 
@@ -9,23 +9,45 @@ root = Tk()
 root.geometry("700x500")
 root.title("QR CODE")
 
-data = "my name is Umer"
+def qr_generator():
+    global Image
+    data = qr_data.get()
+    qr = qrcode.QRCode(version = 1, box_size = 6, border = 4)
+    qr.add_data(data)
+    qr.make(fit = True)
 
-qr = qrcode.QRCode(version = 1, box_size = 6, border = 4)
-qr.add_data(data)
-qr.make(fit = True)
-name = "check.png"
+    name = qr_name.get()+".png"
 
-qr_img = qr.make_image(fill_color= "black", back_color ="white")
+    qr_img = qr.make_image(fill_color= "black", back_color ="white")
 
-qr_img.save(name)
+    qr_img.save(f'.\pics\{name}')
 
-lab = Label(root)
-lab.pack()
+    Image = PhotoImage(file=f'.\pics\{name}')
+    qr_img_lab.config(image=Image)
 
-Image = PhotoImage(file=f'{name}')
+s = Style()
+s.configure("TFrame", background="black", foreground= "white",padx= 50)
+note = Notebook(root,style="TFrame")
+tab1 = Frame(note,style="TFrame")
+tab2 = Frame(note,style="TFrame")
+note.add(tab1, text='QR CODE GENERATOR')
+note.add(tab2, text="QR CODE DECODER")
+note.pack(fill=BOTH)
 
-lab.config(image=Image)
+can1 = Canvas(tab1, width=500,height=480,borderwidth=4,relief=SOLID)
+can1.pack()
+qr_img_lab = Label(root,borderwidth=2,relief=SOLID)
 
+can1.create_window(250,150,window=qr_img_lab)
+
+qr_name = Entry(root)
+qr_data = Entry(root)
+btn = Button(root,command=qr_generator)
+can1.create_window(250,300,window=qr_data)
+can1.create_window(250,350,window=qr_name)
+can1.create_window(250,400,window=btn)
+
+# lab = Label(root)
+# lab.pack()
 
 root.mainloop()
